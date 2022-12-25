ceef5a4259c884501e87485f830129a71fdbd59b
Shivakant6387 <shivakant5875@gmail.com>
2022-12-25 12:15:40 +0530

    added file

---
+++ src/main/java/com/example/Springbootmultithreading002/Springbootmultithreading002Application.java
@@ -0,0 +1,13 @@
+package com.example.Springbootmultithreading002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootmultithreading002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootmultithreading002Application.class, args);
+	}
+
+}
